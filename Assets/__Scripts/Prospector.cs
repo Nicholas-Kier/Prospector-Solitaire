1856560a7f0063930d6d0bd3f366f959b279ea02
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-11-17 17:48:39 -0500

    First commit
    Adding this to GitHub

---
+++ Assets/__Scripts/Prospector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Prospector : MonoBehaviour
+{
+    static public Prospector S;
+
+    public TextAsset deckXML;
+    
+    [Header("Set Dynamically")]
+    public Deck deck;
+
+    void Awake()
+    {
+        S = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
