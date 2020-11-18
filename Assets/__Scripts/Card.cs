7837e987c1e20fa08de1fb1f8afa16806e8a1cad
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-11-17 19:11:08 -0500

    EOL
    Got to the end, now to debug...

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour
@@ -15,6 +14,85 @@ public class Card : MonoBehaviour
     public GameObject back;
     public CardDefinition def;
 
+    public SpriteRenderer[] spriteRenderers;
+
+    private void Start()
+    {
+        SetSortOrder(0);  // Ensures that the card starts properly depth sorted
+    }
+
+    // If spriteRenderers is not yet defined, this function defines it
+
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty
+
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            // Get SpriteRenderer Components of this GameObject and its children
+
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    // Sets the sortingLayerName on all SpriteRenderer Components
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // Sets the sortingOrder of all SpriteRenderer Components
+
+    public void SetSortOrder(int sOrd)
+    {                                     // a
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderers as tSR
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background
+
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+
+                continue; // And continue to the next iteration of the loop
+            }
+
+            // Each of the children of this GameObject are named
+
+            // switch based on the names
+
+            switch (tSR.gameObject.name)
+            {
+                case "back": // if the name is "back"
+
+                    // Set it to the highest layer to cover the other sprites
+
+                    tSR.sortingOrder = sOrd + 2;
+
+                    break;
+
+                case "face":  // if the name is "face"
+
+                default:      //  or if it's anything else
+
+                    // Set it to the middle layer to be above the background
+
+                    tSR.sortingOrder = sOrd + 1;
+
+                    break;
+            }
+        }
+    }
+
     public bool faceUp
     {
         get
@@ -25,6 +103,11 @@ public class Card : MonoBehaviour
         {
             back.SetActive(!value);
         }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); // When clicked, this outputs the card name
     }
 }
 
---
+++ Assets/__Scripts/ScoreManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public enum eScoreEvent
+{
+    draw,
+    mine,
+    mineGold,
+    gameWin,
+    gameLoss
+}
+
+public class ScoreManager : MonoBehaviour
+{
+    static private ScoreManager S;
+
+    static public int SCORE_FROM_PREV_ROUND = 0;
+    static public int HIGH_SCORE = 0;
+
+    [Header("set dyanmically")]
+    public int chain = 0;
+
+    public int scoreRun = 0;
+    public int score = 0;
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("already set boo awake");
+        }
+        if (PlayerPrefs.HasKey("ProspectorHighScore"))
+        {
+            HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
+        }
+        score += SCORE_FROM_PREV_ROUND;
+        SCORE_FROM_PREV_ROUND = 0;
+    }
+
+    static public void EVENT(eScoreEvent evt)
+    {
+        try
+        {  // try-catch stops an error from breaking your program
+            S.Event(evt);
+        }
+        catch (System.NullReferenceException nre)
+        {
+            Debug.LogError("ScoreManager.EVENT() called while S=null.\n" + nre);
+        }
+    }
+
+    private void Event(eScoreEvent evt)
+    {
+        switch (evt)
+        {
+            case eScoreEvent.draw:
+            case eScoreEvent.gameWin:
+            case eScoreEvent.gameLoss:
+                chain = 0;
+                score += scoreRun;
+                scoreRun = 0;
+                break;
+
+            case eScoreEvent.mine:
+                chain++;
+                scoreRun += chain;
+                break;
+        }
+        switch (evt)
+        {
+            case eScoreEvent.gameWin:
+                SCORE_FROM_PREV_ROUND = score;
+                print("you won round score" + score);
+                break;
+
+            case eScoreEvent.gameLoss:
+                if (HIGH_SCORE <= score)
+                {
+                    print("you got high score" + score);
+                    HIGH_SCORE = score;
+                    PlayerPrefs.SetInt("ProspectorHighScore", score);
+                }
+                else
+                {
+                    print("your final score was " + score);
+                }
+                break;
+
+            default:
+                print("score: " + score + "scoreRun " + scoreRun + "Chain " + chain);
+                break;
+        }
+    }
+
+    static public int CHAIN { get { return S.chain; } }
+    static public int SCORE { get { return S.score; } }
+    static public int SCORE_RUN { get { return S.scoreRun; } }
+}
