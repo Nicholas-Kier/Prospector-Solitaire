d7a29bec96b8480eecf6d47407b72a8965948e47
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-11-19 19:18:06 -0500

    Commits

--- Assets/__Scripts/ScoreManager.cs
+++ Assets/__Scripts/ScoreManager.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+//enum to handle all the possible scoring events
 public enum eScoreEvent
 {
     draw,
@@ -11,54 +14,62 @@ public enum eScoreEvent
 
 public class ScoreManager : MonoBehaviour
 {
-    static private ScoreManager S;
+    private static ScoreManager S;
 
-    static public int SCORE_FROM_PREV_ROUND = 0;
-    static public int HIGH_SCORE = 0;
+    public static int SCORE_FROM_PREV_ROUND = 0;
+    public static int HIGH_SCORE = 0;
 
-    [Header("set dyanmically")]
+    [Header("Set Dynamically")]
     public int chain = 0;
-
     public int scoreRun = 0;
     public int score = 0;
 
-    private void Awake()
+    void Awake()
     {
         if (S == null)
         {
-            S = this;
+            S = this; //set the private singleton
         }
+
         else
         {
-            Debug.LogError("already set boo awake");
+            Debug.Log("ERROR: ScoreManager.Aware(): S is already set");
         }
+
+        //check for a high score in the PlayerPrefs
         if (PlayerPrefs.HasKey("ProspectorHighScore"))
         {
             HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
         }
-        score += SCORE_FROM_PREV_ROUND;
+
+        //add the score from last round, which will be > 0 if it was a win
+        score = score + SCORE_FROM_PREV_ROUND;
+
+        //and reset the SCORE_FROM_PREV_ROUND 
         SCORE_FROM_PREV_ROUND = 0;
     }
 
-    static public void EVENT(eScoreEvent evt)
+    public static void EVENT(eScoreEvent evt)
     {
         try
-        {  // try-catch stops an error from breaking your program
+        { //try-catch stops an error from breaking your program
             S.Event(evt);
         }
+
         catch (System.NullReferenceException nre)
         {
-            Debug.LogError("ScoreManager.EVENT() called while S=null.\n" + nre);
+            Debug.LogError("ScoreManager.EVENT() called while s=null.\n" + nre);
         }
     }
 
-    private void Event(eScoreEvent evt)
+    void Event(eScoreEvent evt)
     {
         switch (evt)
         {
-            case eScoreEvent.draw:
-            case eScoreEvent.gameWin:
-            case eScoreEvent.gameLoss:
+            //same things need to happen whether it's a draw, a win, or a lose
+            case eScoreEvent.draw: //drawing the card
+            case eScoreEvent.gameWin: //won the round
+            case eScoreEvent.gameLoss: //lost the round
                 chain = 0;
                 score += scoreRun;
                 scoreRun = 0;
@@ -69,28 +80,37 @@ public class ScoreManager : MonoBehaviour
                 scoreRun += chain;
                 break;
         }
+
+        //this second switch statement handles round wins and loses
         switch (evt)
         {
             case eScoreEvent.gameWin:
+
+                //if its a win, add the score to the next round
+                //static fields are NOT reset by SceneManager.LoadScene()
                 SCORE_FROM_PREV_ROUND = score;
-                print("you won round score" + score);
+                print("You won this round! Round Score " + score);
                 break;
 
             case eScoreEvent.gameLoss:
+
+                //if its a loss, check against the high score
                 if (HIGH_SCORE <= score)
                 {
-                    print("you got high score" + score);
+                    print("You got the high score! High Score: " + score);
                     HIGH_SCORE = score;
                     PlayerPrefs.SetInt("ProspectorHighScore", score);
                 }
+
                 else
                 {
-                    print("your final score was " + score);
+                    print("Your final score for this game was: " + score);
                 }
+
                 break;
 
             default:
-                print("score: " + score + "scoreRun " + scoreRun + "Chain " + chain);
+                print("score: " + score + " scoreRun: " + scoreRun + " chain: " + chain);
                 break;
         }
     }
